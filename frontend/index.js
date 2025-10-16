c782fd8fb05b02e430795b394f2ed8d57f095611
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-16 15:06:55 -0500

    Se corrige error de usar app.set con la ruta de estaticos a app.use

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,4 @@
 import exp from 'express'
-import { landingPage } from './controllers/landingPage.controller.js'
 import routes from './routes/landingPage.router.js'
 import path from 'path'
 process.loadEnvFile('../.env')
@@ -9,8 +8,8 @@ const PORT = process.env.PORT_FRONT
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(process.cwd(), 'views'))
-app.set('/static', exp.static(path.join(process.cwd(), 'public')))
 
+app.use('/static', exp.static(path.join(process.cwd(), 'public')))
 app.use('/', routes)
 
 app.listen(PORT, () => console.log(`Frontend escuchando en http://localhost:${PORT}`))
