85e4247bddd837b0441a5ecd2702b0b3f0fd5551
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-16 15:04:48 -0500

    se cambia la manera de llamar al error

--- frontend/controllers/landingPage.controller.js
+++ frontend/controllers/landingPage.controller.js
@@ -2,10 +2,10 @@ import api from '../services/backend.service.js'
 
 export async function landingPage(req, res) {
     try {
-        const landing = await api.get('/')
+        const landing = await api.get('/ofertas')
         res.render('pages/landingPage', {landing, title: 'WorkHub - Inicio'})
     } catch (error) {
-        console.error('Error al renderizar la landingPage', error?.message)
+        console.error('Error al renderizar la landingPage', error.message)
         res.status(500).render('pages/landingPage', {landing: null, title: 'Workhub - Inicio', error: 'No se pudo cargar la landing page'})
     }
 }
