a3eabc3a9d2d1c36575c54b4b3ecc90c9c402f09
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-16 02:00:32 -0500

    Se crea el archivo principal del frontend

---
+++ frontend/index.js
@@ -0,0 +1,16 @@
+import exp from 'express'
+import { landingPage } from './controllers/landingPage.controller.js'
+import routes from './routes/landingPage.router.js'
+import path from 'path'
+process.loadEnvFile('../.env')
+
+const app = exp()
+const PORT = process.env.PORT_FRONT
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(process.cwd(), 'views'))
+app.set('/static', exp.static(path.join(process.cwd(), 'public')))
+
+app.use('/', routes)
+
+app.listen(PORT, () => console.log(`Frontend escuchando en http://localhost:${PORT}`))
