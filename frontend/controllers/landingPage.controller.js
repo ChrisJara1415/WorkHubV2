ae0d0120832578e1dfd426d86bc73d208718e285
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-16 01:58:30 -0500

    Se crea el archivo controlador de la landing page

---
+++ frontend/controllers/landingPage.controller.js
@@ -0,0 +1,11 @@
+import api from '../services/backend.service.js'
+
+export async function landingPage(req, res) {
+    try {
+        const landing = await api.get('/')
+        res.render('pages/landingPage', {landing, title: 'WorkHub - Inicio'})
+    } catch (error) {
+        console.error('Error al renderizar la landingPage', error?.message)
+        res.status(500).render('pages/landingPage', {landing: null, title: 'Workhub - Inicio', error: 'No se pudo cargar la landing page'})
+    }
+}
