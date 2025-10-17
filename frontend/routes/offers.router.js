9978f57f99d0ff6e3a3d559885a186371b17585a
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-17 00:50:38 -0500

    Se crean las rutas para las ofertas

--- frontend/routes/offers.router.js
+++ frontend/routes/offers.router.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { landingPage } from '../controllers/landingPage.controller.js'
 import { offers } from '../controllers/offers.controller.js'
 
 const router = express.Router()
 
-router.get('/', landingPage)
+router.get('/ofertas', offers)
 
 export default router
