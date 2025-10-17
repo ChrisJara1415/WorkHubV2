3284695f37828f89a9691f3769b67c07aae30103
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-16 21:50:05 -0500

    Se agrega el controlador de las ofertas

--- frontend/controllers/landingPage.controller.js
+++ frontend/controllers/landingPage.controller.js
@@ -2,10 +2,20 @@ import api from '../services/backend.service.js'
 
 export async function landingPage(req, res) {
     try {
-        const landing = await api.get('/ofertas')
+        const landing = await api.get('/')
         res.render('pages/landingPage', {landing, title: 'WorkHub - Inicio'})
     } catch (error) {
         console.error('Error al renderizar la landingPage', error.message)
         res.status(500).render('pages/landingPage', {landing: null, title: 'Workhub - Inicio', error: 'No se pudo cargar la landing page'})
+    }
+}
+
+export async function offers(req, res) {
+    try {
+        const offers = await api.get('/ofertas')
+        res.render('pages/offers', {offers, title: 'Ofertas Disponibles'})
+    } catch (error) {
+        console.error('Error al renderizar las ofertas', error.message)
+        res.status(500).render('pages/offers', {offers: [], title: 'Ofertas Disponibles', error: 'No se pudo cargar las ofertas'})
     }
 }
