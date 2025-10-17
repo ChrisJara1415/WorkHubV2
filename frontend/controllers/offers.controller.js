bf280dedfe7bb7c531a6712180ff5958c396d34f
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-17 01:59:26 -0500

    Se pasa las categorías para renderizarlas en la vista

--- frontend/controllers/offers.controller.js
+++ frontend/controllers/offers.controller.js
@@ -1,11 +1,19 @@
 import api from '../services/backend.service.js'
 
+const CATEGORIES = [
+    { name: 'Jardinería', icon: 'bi-tree' },
+    { name: 'Limpieza', icon: 'bi-droplet' },
+    { name: 'Piscinero', icon: 'bi-water' },
+    { name: 'Carpintería', icon: 'bi-hammer' },
+    { name: 'Mantenimiento', icon: 'bi-wrench' },
+    { name: 'Plomería', icon: 'bi-tools' }
+];
 export async function offers(req, res) {
     try {
         const offers = await api.get('/ofertas')
-        res.render('pages/offers', {offers, title: 'Ofertas Disponibles'})
+        res.render('pages/offers', { categorias: CATEGORIES, offers, title: 'Ofertas Disponibles' })
     } catch (error) {
         console.error('Error al renderizar las ofertas', error.message)
-        res.status(500).render('pages/offers', {offers: [], title: 'Ofertas Disponibles', error: 'No se pudo cargar las ofertas'})
+        res.status(500).render('pages/offers', { categorias: CATEGORIES, offers: [], title: 'Ofertas Disponibles', error: 'No se pudo cargar las ofertas' })
     }
 }
