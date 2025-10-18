f14ed72dd70974cd81c1c2ec8f3602000bfd5ba9
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-17 20:55:35 -0500

    Se importa la ruta de empleador

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 import exp from 'express'
+import employerDash from './routes/employerDash.router.js'
 import landingPage from './routes/landingPage.router.js'
 import offersRoutes from './routes/offers.router.js'
 import path from 'path'
@@ -15,5 +16,6 @@ app.use(morgan('dev'))
 app.use('/static', exp.static(path.join(process.cwd(), 'public')))
 app.use('/', landingPage)
 app.use('/ofertas', offersRoutes)
+app.use('/empleador', employerDash)
 
 app.listen(PORT, () => console.log(`Frontend escuchando en http://localhost:${PORT}`))
