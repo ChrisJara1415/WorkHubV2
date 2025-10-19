1dab24ec597a682e7d1c633afa906b6eb6185c26
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-18 19:28:53 -0500

    Se agrega el enrutador para traer los detalles de las rutas

--- frontend/routes/offers.router.js
+++ frontend/routes/offers.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { offers } from '../controllers/offers.controller.js'
+import { offers, offersDetails } from '../controllers/offers.controller.js'
 
 const router = express.Router()
 
 router.get('/', offers)
+router.get('/:id', offersDetails)
 
 export default router
