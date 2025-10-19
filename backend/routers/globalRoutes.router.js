b9ca2f5944e3ff7c72d0be0ac9fd79950486fe8a
ChrisJara1415 <chris.jara0626@gmail.com>
2025-10-19 00:45:31 -0500

    Se importa el login

--- backend/routers/globalRoutes.router.js
+++ backend/routers/globalRoutes.router.js
@@ -2,10 +2,12 @@ import usersRouter from './users.router.js'
 import offersRouter from './offers.router.js'
 import contractsRouter from './contracts.router.js'
 import applymentsRouter from './applyments.router.js'
+import { login } from '../controllers/users.controller.js'
 import { Router } from 'express'
 
 const router = Router()
 
+router.post('/login', login)
 router.use('/clientes', usersRouter)
 router.use('/ofertas', offersRouter)
 router.use('/contratos', contractsRouter)
